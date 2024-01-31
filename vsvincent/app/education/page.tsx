84e980babfd0bc003d9ddd701c5d7f45ef64611d
vsvincent <vincentvs01@yahoo.com>
2024-02-01 00:32:31 +0100

    Add education start and end

--- vsvincent/app/education/page.tsx
+++ vsvincent/app/education/page.tsx
@@ -6,11 +6,13 @@ import { Card } from "../components/card";
 
 const schools = [
 	{
+		start: "2020",
+		end: "2024",
 		startIcon: <School2 size={20} />,
 		endIcon: <GraduationCap size={20} />,
 		href: "https://www.fontys.nl/en/Study-at-Fontys/Programmes/Information-Communication-Technology-Eindhoven.htm",
 		name: "Fontys University of Applied Science",
-		degree: "Information and Communication Technology",
+		degree: "Information and Communication Technology: Software Engineering",
 	}
 ];
 
@@ -27,22 +29,26 @@ export default function Example() {
 								target="_blank"
 								className="p-4 relative flex flex-col items-center gap-4 duration-700 group md:gap-8 md:py-24  lg:pb-48  md:p-16"
 							>
-								<div className="flex w-full justify-center items-center">
-									<span className="relative z-10 flex items-center justify-center w-12 h-12 text-sm duration-1000 border rounded-full text-zinc-200 group-hover:text-white group-hover:bg-zinc-900 border-zinc-500 bg-zinc-900 group-hover:border-zinc-200 drop-shadow-orange">
-										{s.startIcon}
-									</span>
+								<div className="flex w-full justify-center items-center flex-col">
+									<div className="flex w-full justify-center items-center">
+										<span className="relative z-10 flex flex-col items-center justify-center w-12 h-12 text-sm duration-1000 border rounded-full text-zinc-200 group-hover:text-white group-hover:bg-zinc-900 border-zinc-500 bg-zinc-900 group-hover:border-zinc-200 drop-shadow-orange">
+											{s.startIcon}
+											<p className="p-1">{s.start}</p>
+										</span>
 
-									<div className="relative flex items-center w-2/4 h-px">
-										<span
-										className="absolute w-full h-px bg-gradient-to-r from-zinc-500 via-zinc-500/95 to-transparent"
-										aria-hidden="true"
-										/>
-									</div>
+										<div className="relative flex items-center w-2/4 h-px">
+											<span
+											className="absolute w-full h-px bg-gradient-to-r from-zinc-500 via-zinc-500/95 to-transparent"
+											aria-hidden="true"
+											/>
+										</div>
 
-									<span className="relative z-10 flex items-center justify-center w-12 h-12 text-sm duration-1000 border rounded-full text-zinc-200 group-hover:text-white group-hover:bg-zinc-900 border-zinc-500 bg-zinc-900 group-hover:border-zinc-200 drop-shadow-orange">
-										{s.endIcon}
-									</span>
-								</div>{" "}
+										<span className="relative z-10 flex flex-col items-center justify-center w-12 h-12 text-sm duration-1000 border rounded-full text-zinc-200 group-hover:text-white group-hover:bg-zinc-900 border-zinc-500 bg-zinc-900 group-hover:border-zinc-200 drop-shadow-orange">
+											{s.endIcon}
+											<p className="p-1">{s.end}</p>
+										</span>
+									</div>{" "}
+								</div>
 								<div className="z-10 flex flex-col items-center">
 									<span className="lg:text-xl text-center font-medium duration-150 xl:text-3xl text-zinc-200 group-hover:text-white font-display">
 										{s.degree}
