3a12198f7c2639be4ac04c5ec0f3b442e20eed0d
vsvincent <vincentvs01@yahoo.com>
2024-02-10 17:42:54 +0100

    Remove contentlayer references

--- vsvincent/contentlayer.config.js
+++ vsvincent/contentlayer.config.js
@@ -1,10 +1,8 @@
-import { defineDocumentType, makeSource } from "contentlayer/source-files";
 import remarkGfm from "remark-gfm";
 import rehypePrettyCode from "rehype-pretty-code";
 import rehypeSlug from "rehype-slug";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 
-/** @type {import('contentlayer/source-files').ComputedFields} */
 const computedFields = {
 	path: {
 		type: "string",
