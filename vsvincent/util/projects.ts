1f915db612841c45ebdb40b7405bf4b375ecb81d
vsvincent <vincentvs01@yahoo.com>
2024-02-11 15:16:27 +0100

    Expand markdown descriptions

--- vsvincent/util/projects.ts
+++ vsvincent/util/projects.ts
@@ -9,7 +9,7 @@ export interface Project {
     url?: string;
     repository?: string;
   }
-  //NOTE: **Do NOT indent the markdown as it will break text-wrapping**
+  //NOTE: **Do NOT indent the markdown as it will break text-wrapping**...Learned this the hard way
   const projects: Project[] = [
     {
         slug: "isnad",
@@ -19,7 +19,7 @@ export interface Project {
 markdown: 
 `
 # What are Hadith
-Hadith are narrations or sayings attributed to the Prophet Muhammad, which are crucial in Islamic jurisprudence and elaborating details of creed. The authenticity and reliability of Hadith are determined in part by the chain of transmission (isnad) through which they were passed down.
+Hadith are narrations or sayings attributed to the Prophet Muhammad, which are crucial in Islamic jurisprudence and elaborating details of creed. The authenticity and reliability of Hadith are determined in part by the chain of transmission (Isnad) through which they were passed down.
 ## Why Isnad
 Isnad will provide users with tools to create and explore graph-based maps of these transmission chains. This includes visualizing the connections between various narrators (transmitters) and the Hadith they transmitted. Additionally, the software may incorporate features for annotating and analyzing the reliability, context, and other pertinent information related to each transmitter and Hadith.
 
@@ -37,8 +37,10 @@ Furthermore, Isnad might also appeal to Islamic scholars, imams, and educators w
         title: 'DBMS Performance Tester',
         imageUrl: '/images/project2.jpg',
         description: `A framework to run and visualize performance tests to evaluate the viability of various alternative databases in the context of Minut's property monitoring system at scale.`,
-        markdown: `A framework to run and visualize performance tests to evaluate the viability of various alternative databases in the context of the enterprise system.
-Helped to create a plan reporting on the sustainability of the company database for future scaling and gave professional advice on potential mitigation strategies.`,
+markdown: `
+A framework to run and visualize performance tests to evaluate the viability of various alternative databases in the context of the enterprise system.
+Helped to create a plan reporting on the sustainability of the company database for future scaling and gave professional advice on potential mitigation strategies.
+`,
         published: true,
         date: new Date('2024-01-19'),
     },
@@ -47,7 +49,9 @@ Helped to create a plan reporting on the sustainability of the company database 
         title: 'Open Journalism',
         imageUrl: '/images/project2.jpg',
         description: 'A project aimed at enhancing the collaboration process within a journalistic ecosystem.',
-        markdown: `A project aimed at enhancing the collaboration process within a journalistic ecosystem by creating a scalable, event-based system for future use by the university's journalism study student body.`,
+markdown: `
+A project aimed at enhancing the collaboration process within a journalistic ecosystem by creating a scalable, event-based system for future use by the university's journalism study student body.
+`,
         published: true,
         date: new Date('2023-05-30'),
         url: 'https://www.fontysictinnovationlab.nl/innovations-insight/innovations-insight-june-23/ict-software-engineering/open-journalism-infrastructure/'
