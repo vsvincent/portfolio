781e8f314911abe998daa6720ea91c51c24dd47a
vsvincent <vincentvs01@yahoo.com>
2024-02-11 15:04:05 +0100

    Add bottom screen fade effect

--- vsvincent/app/projects/projectBody.tsx
+++ vsvincent/app/projects/projectBody.tsx
@@ -19,6 +19,9 @@ type Props = {
 
 export const ProjectBody: React.FC<Props> = ({ project, views, setProject }) => {
 	return (
-		<Markdown className="flex-1 relative h-screen isolate overflow-y-visible bg-gradient-to-tl from-slate-50 via-slate-100 to-slate-300 p-5 md:p-10 lg:p-15">{project.markdown}</Markdown>
+		<div className="flex-1 relative h-screen isolate overflow-y-auto scrollbar-hide bg-gradient-to-tl from-slate-50 via-slate-100 to-slate-300 p-5 md:p-10 lg:p-15">
+			<div className="fixed bg-gradient-to-b from-transparent to-slate-50 z-3 bottom-0 left-0 w-full h-5"></div>
+			<Markdown className="z-[-1]">{project.markdown}</Markdown>
+		</div>
 	);
 };
