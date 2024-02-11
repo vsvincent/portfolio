a01e558dea45a44df5f8c49808c616fe26ff72d5
vsvincent <vincentvs01@yahoo.com>
2024-02-11 12:57:54 +0100

    Adjust projects for markdown usage

--- vsvincent/util/projects.ts
+++ vsvincent/util/projects.ts
@@ -3,41 +3,46 @@ export interface Project {
     title: string;
     imageUrl: string;
     description: string;
-    shortDescription?: string;
+    markdown: string;
     published: boolean;
     date: Date;
     url?: string;
     repository?: string;
   }
-
+  //NOTE: **Do NOT indent the markdown as it will break text-wrapping**
   const projects: Project[] = [
     {
         slug: "isnad",
-        title: 'Project 1',
+        title: 'Isnad.app',
         imageUrl: '/images/project1.jpg',
-        description: 'Description of Project 1',
-        shortDescription: 'Shorter Description of Project 2',
+        description: 'A web project recording chains of transmission and their transmitters, particularly in the English language.',
+        markdown: `## Why Isnad
+Isnad will allow users to help create a graph-based map of Hadith transmission chains as well as their transmitters and information relevant to them.
+Hadith are narrations or sayings attributed to the Prophet Muhammad, which are crucial in Islamic jurisprudence and theology. The authenticity and reliability of Hadith are determined in part by the chain of transmission (isnad) through which they were passed down.`,
         published: true,
         date: new Date('2023-12-01'),
         repository: 'vsvincent/isnad-backend',
+        url: 'https://isnad.app'
     },
     {
         slug: "databases",
-        title: 'Project 2',
+        title: 'DBMS Performance Tester',
         imageUrl: '/images/project2.jpg',
-        description: 'Description of Project 2',
-        shortDescription: 'Shorter Description of Project 2',
+        description: `A framework to run and visualize performance tests to evaluate the viability of various alternative databases in the context of Minut's property monitoring system at scale.`,
+        markdown: `A framework to run and visualize performance tests to evaluate the viability of various alternative databases in the context of the enterprise system.
+Helped to create a plan reporting on the sustainability of the company database for future scaling and gave professional advice on potential mitigation strategies.`,
         published: true,
         date: new Date('2024-01-19'),
     },
     {
         slug: "openjournalism",
-        title: 'Project 2',
+        title: 'Open Journalism',
         imageUrl: '/images/project2.jpg',
-        description: 'Description of Project 2',
-        shortDescription: 'Shorter Description of Project 2',
+        description: 'A project aimed at enhancing the collaboration process within a journalistic ecosystem.',
+        markdown: `A project aimed at enhancing the collaboration process within a journalistic ecosystem by creating a scalable, event-based system for future use by the university's journalism study student body.`,
         published: true,
         date: new Date('2023-05-30'),
+        url: 'https://www.fontysictinnovationlab.nl/innovations-insight/innovations-insight-june-23/ict-software-engineering/open-journalism-infrastructure/'
     },
   ];
   export const getAllProjects = (): Project[] => {
