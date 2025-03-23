8fdea7edf3509f1fb06f07f0a69edf596e5c3067
vsvincent <vincentvs01@yahoo.com>
2025-03-23 17:07:18 +0100

    fix: fix text visibility on smaller screens

--- vsvincent/app/page.tsx
+++ vsvincent/app/page.tsx
@@ -16,24 +16,24 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
-      
+
       <Particles
         className="absolute inset-0 z-10 animate-fade-in pointer-events-none"
         quantity={100}
       />
-     
+
       <LampContainer>
-      <h1 className="z-1 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
-        vsvincent
-      </h1>
-      <div className="my-16 text-center animate-fade-in">
-        <h2 className="text-sm text-zinc-500 ">
-          I love designing and building software that helps sustain society and move it forward.
-        </h2>
-      </div>
-      <BreakLine />
-      <HeroNav navigation={navigation} />
-      <BreakLine fadeLeft={true}/>
+        <h1 className="z-1 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
+          vsvincent
+        </h1>
+        <div className="my-16 text-center animate-fade-in">
+          <h2 className="text-sm text-zinc-400 ">
+            Improve lives, bridge talents, and solve problems with software.
+          </h2>
+        </div>
+        <BreakLine />
+        <HeroNav navigation={navigation} />
+        <BreakLine fadeLeft={true} />
       </LampContainer>
     </div>
   );
